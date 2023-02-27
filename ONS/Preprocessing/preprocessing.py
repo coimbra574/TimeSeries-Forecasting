44a06fc177a549cd1fc4cc0de75dc1c506772e2b
coimbra574 <tainacoimbra1996@gmail.com>
2023-02-26 21:19:04 -0300

    import dataset

--- ONS/Preprocessing/preprocessing.py
+++ ONS/Preprocessing/preprocessing.py
@@ -4,3 +4,9 @@ import matplotlib.pyplot as plt
 import os
 from datetime import datetime
 from statsmodels.tsa.seasonal import seasonal_decompose
+
+os.chdir('/content/drive/MyDrive/Mestrado/Dados_ONS/Dados ONS')
+df = pd.DataFrame()
+for file in os.listdir():
+    data = pd.read_excel(file)
+    df = df.append(data)
